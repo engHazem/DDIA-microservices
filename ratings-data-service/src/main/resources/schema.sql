121896ffc6a750a8a580a5069b0fe34c11d9e486
Habiba abdel hameed <154561371+habibarezq@users.noreply.github.com>
2026-04-08 00:15:42 +0200

    added a comment to insert records

--- ratings-data-service/src/main/resources/schema.sql
+++ ratings-data-service/src/main/resources/schema.sql
@@ -7,4 +7,32 @@ CREATE TABLE IF NOT EXISTS ratings(
     rating INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+-- insert records 
+USE ratings_db;
 
+-- Insert 100 users with random ratings
+INSERT INTO ratings (user_id, movie_id, rating)
+VALUES
+-- -- Loop over users
+-- USE ratings_db;
+
+-- INSERT INTO ratings (user_id, movie_id, rating)
+-- VALUES
+-- ('user1', 'movie1', FLOOR(1 + RAND()*5)),
+-- ('user1', 'movie2', FLOOR(1 + RAND()*5)),
+-- ('user1', 'movie3', FLOOR(1 + RAND()*5)),
+-- ('user1', 'movie4', FLOOR(1 + RAND()*5)),
+-- ('user1', 'movie5', FLOOR(1 + RAND()*5)),
+
+-- ('user2', 'movie3', FLOOR(1 + RAND()*5)),
+-- ('user2', 'movie7', FLOOR(1 + RAND()*5)),
+-- ('user2', 'movie2', FLOOR(1 + RAND()*5)),
+-- ('user2', 'movie10', FLOOR(1 + RAND()*5)),
+-- ('user2', 'movie5', FLOOR(1 + RAND()*5)),
+
+-- -- Continue similarly up to user100
+-- ('user100', 'movie1', FLOOR(1 + RAND()*5)),
+-- ('user100', 'movie8', FLOOR(1 + RAND()*5)),
+-- ('user100', 'movie12', FLOOR(1 + RAND()*5)),
+-- ('user100', 'movie6', FLOOR(1 + RAND()*5)),
+-- ('user100', 'movie19', FLOOR(1 + RAND()*5));
