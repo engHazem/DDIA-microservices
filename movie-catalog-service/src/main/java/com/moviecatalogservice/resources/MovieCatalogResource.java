765cae1853ff54c0ca596d4c973aa75c08eb6169
Abdelrahman Reda <abdelrahmanredafawzy@gmail.com>
2026-04-08 19:11:43 +0200

    Add gRPC trending movies client and proto

--- movie-catalog-service/src/main/java/com/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,39 +1,33 @@
 package com.moviecatalogservice.resources;
 
 import com.moviecatalogservice.models.CatalogItem;
-import com.moviecatalogservice.models.Movie;
 import com.moviecatalogservice.models.Rating;
-import com.moviecatalogservice.models.UserRating;
 import com.moviecatalogservice.services.MovieInfoService;
+import com.moviecatalogservice.services.TrendingMoviesGrpcClient;
 import com.moviecatalogservice.services.UserRatingService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
-    private final RestTemplate restTemplate;
-
     private final MovieInfoService movieInfoService;
 
     private final UserRatingService userRatingService;
 
-    public MovieCatalogResource(RestTemplate restTemplate,
-                                MovieInfoService movieInfoService,
-                                UserRatingService userRatingService) {
+    private final TrendingMoviesGrpcClient trendingMoviesGrpcClient;
 
-        this.restTemplate = restTemplate;
+    public MovieCatalogResource(MovieInfoService movieInfoService,
+                                UserRatingService userRatingService,
+                                TrendingMoviesGrpcClient trendingMoviesGrpcClient) {
         this.movieInfoService = movieInfoService;
         this.userRatingService = userRatingService;
+        this.trendingMoviesGrpcClient = trendingMoviesGrpcClient;
     }
 
     /**
@@ -47,5 +41,10 @@ public class MovieCatalogResource {
     public List<CatalogItem> getCatalog(@PathVariable String userId) {
         List<Rating> ratings = userRatingService.getUserRating(userId).getRatings();
         return ratings.stream().map(movieInfoService::getCatalogItem).collect(Collectors.toList());
+    }
+
+    @RequestMapping("/trending")
+    public List<String> getTrendingMovies() {
+        return trendingMoviesGrpcClient.getTopMovieIds();
     }
 }
